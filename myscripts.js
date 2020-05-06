a43b5cdc2bf86567dacbb36799e061e8c328f4b9
rajujnvgupta <39230239+rajujnvgupta@users.noreply.github.com>
2020-05-06 19:49:35 +0530

    first commit

--- myscripts.js
+++ myscripts.js
@@ -1,28 +1,10 @@
 
 var i = 0;
-var nameOfUser = "john";
-
-//  var txt = "Happy Birthday " + nameOfUser  + " you are " + age  + ", years old! You were born on this day, in the year " + dateOfBirth + ".";
-var txt = "Happy Birthday" + " Happy";
-var s = 100;
-
-function myFunction() {
-  var x, text;
-  // Get the value of the input field with id="numb"
-  x = document.getElementById("numb").value;
-  // If x is Not a Number or less than one or greater than 10
-  if (isNaN(x) || x < 1 || x > 10){
-    text = "Input not valid";
-  } else {
-    text = "Input OK";
-  }
-  i = 0;
-  if(i < txt.length()){
-		document.getElementById("demo").innerHTML += txt.charAt(i);
-		i++;
-		// setTimeout(typerwriter, s);
-	}
-}
+var options = ["I hope you have a wonderfull day, Shreya:). and you dreams comes true this years", "Age is just a number representing how long the world has been enjoying you.", "Now you are 32 years old! Sorry 22 year :). You were born on this day, in 1998", "Tell me Exact number, How old You are :), I know 7 may 1998 is just for Educational Purpose :). Any way number doesn't matter here :) .", "You think you’re something special because it’s your birthday today? You’re something special every day! :)", "I figured out, what’s the most challenging thing to do. I think it’s the counting of your wrinkles. Finding one is impossible. It is my bad, i don't even have your PHOTOS"];
+var len = options.length;
+var idx = Math.floor(Math.random() * len);
+var txt = options[idx];
+var s = 100; //timer delay
 
 function typerwriter(){
 	if(i < txt.length){
